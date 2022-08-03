e0f717944d72930d3f6178d86055581f250a06d3
Dan Phillips <dan.phillips100@gmail.com>
2022-08-03 09:54:35 +0100

    feat: Gameboard creation and CSS  added

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello")
+const gameBoard = (() => { 
+
+  // Create empty array for new game
+  let board = Array(9).fill('')
+
+  // Create and display squares on the DOM for each array index
+  let cells = document.querySelector("#gameBoard")
+  board.forEach(() => {
+    const cell = document.createElement("div")
+    cell.className = "square"
+    cells.appendChild(cell)
+  })
+
+  return {
+    board
+  }
+
+})()
